8fb3267704c91e42c7f358877c243c17a5445b33
Shefali Tyagi <shefalityagi@Shefalis-MacBook-Pro.local>
2022-04-17 12:06:11 +0530

    adding user sign in and sign up page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,5 +3,17 @@ module.exports.profile = function(req,res){
     res.render('users', {
         
         title: "Users"
-    });
+    })
+}
+//render the sign up page
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up', {
+        title: "Codeial|Sign Up"
+    })
+}
+//render the sign in page
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in', {
+        title: "Codeial|Sign In"
+    })
 }
