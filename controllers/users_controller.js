f8012f52a4a55b52536a6541f5443672e095409c
Shefali Tyagi <shefalityagi@Shefalis-MacBook-Pro.local>
2022-04-22 20:06:25 +0530

    sign up part done

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 module.exports.profile = function(req,res){
     
     res.render('users', {
@@ -20,7 +22,23 @@ module.exports.signIn = function(req, res){
 
 //get signup data
 module.exports.create = function(req, res){
-    //todo later
+    if (req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('error in finding user in sign up'); return}
+    
+        if(!user){
+            User.create(req.body, function(err, user){
+                if (err){console.log('error in creating user while signing up'); return}
+
+                return res.redirect('/users/sign-in');
+
+            })
+        }else{
+            return res.redirect('back');
+        }
+    });
 }
 //get signin data
 module.exports.createSession = function(req, res){
--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,8 @@ router.get('/sign-up', userController.signUp);
 
 router.get('/sign-in', userController.signIn);
 
+router.post('/create', userController.create);
+
 
 
 module.exports = router;
