a7ecd525ca368c019255decf2d389508296dffc9
Shefali Tyagi <shefalityagi@Shefalis-MacBook-Pro.local>
2022-06-03 21:04:13 +0530

    Deleteing a post ajaz

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -13,6 +13,7 @@
                 success: function(data){
                     let newPost = newPostDom(data.data.post);
                     $('#posts-list-container>ul').prepend(newPost);
+                    deletePost($(' .delete-post-button', newPost));
 
                 }, error: function(error){
                     console.log(error.responseText);
@@ -21,7 +22,7 @@
          });
     
     }
-    createPost();
+    
     //method to create a post in DOM
 
     let newPostDom = function(post){
@@ -29,7 +30,7 @@
         <p>
             
             <small>
-                <a class ="delete-post-button" href = "/posts/destroy/${ post.id }">X</a>
+                <a class ="delete-post-button" href = "/posts/destroy/${ post._id }">X</a>
             </small>
             
             ${post.content}
@@ -54,4 +55,23 @@
         </div>
     </li>`)
     }
+
+    // method to delete a POST from DOM
+    let deletePost = function(deleteLink){
+        $(deleteLink).click(function(e){
+            e.preventDefault();
+
+            $.ajax({
+                type: 'get',
+                url: $(deleteLink).prop('href'),
+                success: function(data){
+                    $(`#post-${data.data.post_id}`).remove();
+                }, error: function(error){
+                    console.log(error.responseText);
+                }
+                
+            });
+        });
+    }
+    createPost();
 }
